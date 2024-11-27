f3b75c657f4cfde481465da0467fad582b44ad40
TomAlcarraz <frantom422@gmail.com>
2024-11-26 21:45:24 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "eks_cluster_name" {
-  value = module.eks.cluster_name
+  value = module.eks.cluster.cluster_name
 }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = module.eks.cluster.endpoint
 }
 
 output "eks_cluster_arn" {
-  value = module.eks.cluster_arn
+  value = module.eks.cluster.arn
 }
